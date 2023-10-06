7776ad57bf983d4812d90e04d17402c9ebdd698d
affanyounus-e64 <113903931+affanyounus-e64@users.noreply.github.com>
2023-10-07 04:04:13 +0500

    Update WeatherForecastController.cs

--- PLMKub/Controllers/WeatherForecastController.cs
+++ PLMKub/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "affan"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "affan", "ammar"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
